4d14a9c8f712639f6aaea64b3b899e2dc864c785
iamjjanga-ouo <namils147@kakao.com>
2021-01-21 11:33:08 +0000

    flask + gunicorn set

--- myproject.py
+++ myproject.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template
 import pymysql
 import os
 
-app = Flask(__name__)
+application = Flask(__name__)
 
 
 db = pymysql.connect( host=os.environ.get('MYSQL_ADDRESS'), 
@@ -11,14 +11,14 @@ db = pymysql.connect( host=os.environ.get('MYSQL_ADDRESS'),
                         db="free_board", 
                         charset="utf8")
 
-@app.route('/')
+@application.route('/')
 def index():
-    return render_template('index.html', data_list = data_list)
-
-if __name__ == '__main__':
     sql = 'SELECT * FROM `board`;'
     cur = db.cursor()
     cur.execute(sql)
     data_list = cur.fetchall()
 
-    app.run(host="0.0.0.0", port=5000)
+    return render_template('index.html', data_list = data_list)
+
+if __name__ == '__main__':
+    application.run(host="127.0.0.1", port=5000)
---
+++ wsgi.py
@@ -0,0 +1,6 @@
+# wsgi.py
+
+from myproject import application
+
+if __name__ == '__main__':
+    application.run()
